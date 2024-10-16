1d44b95f9f5c7f1dafe8191b535d8c917d70c522
AJTimePyro <ajtimepyro@gmail.com>
2024-10-16 18:33:42 +0530

    added register api

---
+++ app/api/auth/register/route.js
@@ -0,0 +1,66 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { prismaDB } from "@/lib/prismaDB";
+import { OTPHandler } from "@/lib/sendEmail";
+
+export async function POST(request) {
+    const body = await request.json();
+    const {email, name, password} = body;
+
+    if (!name || !email || !password) {
+        return NextResponse.json("Missing value name, email or password", {
+                status : 422
+            }
+        )
+    }
+
+    const userExist = await prismaDB.user.findUnique({
+        where : {
+            email
+        }
+    })
+
+    if (userExist) {
+        return new NextResponse("User already exists", {
+            status : 400
+        })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+        const otpClient = new OTPHandler(email)
+        const otp = otpClient.getOTP()
+        const otpExpiry = new Date(Date.now() + 60 * 1000)
+    
+        await prismaDB.nonVerifiedUser.upsert({
+            where : {
+                email
+            },
+            create : {
+                name,
+                email,
+                hashedPassword,
+                otp,
+                otpExpiry
+            },
+            update : {
+                name,
+                hashedPassword,
+                otp,
+                otpExpiry
+            }
+        })
+
+        otpClient.sendOTP()
+        return NextResponse.json("OTP sent, check your email", {
+            status : 200
+        })
+    } catch (err) {
+        console.log(err)
+        return NextResponse.json("Something went wrong", {
+            status : 500
+        })
+    }
+    
+}
