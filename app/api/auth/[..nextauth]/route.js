00f9bf9f2af2cf57e42ee81af063f551e46686b2
AJTimePyro <ajtimepyro@gmail.com>
2024-10-16 18:26:11 +0530

    added auth api route

---
+++ app/api/auth/[..nextauth]/route.js
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth/next";
+import { AuthOptions } from "@/lib/authOptions";
+
+const handler = NextAuth(AuthOptions)
+
+export {
+    handler as GET,
+    handler as POST
+}
