0c2f18dd661a575cc30b3da2009bfeaee4fee633
AJTimePyro <ajtimepyro@gmail.com>
2024-10-18 18:10:57 +0530

    added functions to make it work

--- components/modal.jsx
+++ components/modal.jsx
@@ -1,6 +1,7 @@
 import { X } from "lucide-react"
 import { useState } from "react"
 import InputBox from "./inputBox";
+import { signIn } from "next-auth/react";
 
 const FORM_TYPE = {
     "login": {
@@ -132,15 +133,107 @@ const OTPForm = ({ otp, setOtp }) => {
 }
 
 export const SignupModal = ({ isVisible, onClose }) => {
-    const [formType, setFormType] = useState("otp");
+    const [formType, setFormType] = useState("login");
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [otp, setOtp] = useState("");
 
-    const handleClickableText = () => {
-        if (formType === "otp") {
+    const loginRegisterHandler = async () => {
+        if (formType === "login") {
+            if (!email || !password) {
+                return
+            }
 
+            const res = await signIn("credentials", {
+                email,
+                password,
+                callbackUrl: "/",
+                redirect: false
+            })
+
+        } else if (formType === "register") {
+            if (!email || !password || !username) {
+                return
+            }
+
+            const res = await axios.post(
+                '/api/auth/register',
+                {
+                    email,
+                    username,
+                    password
+                }
+            )
+
+            if (res.status === 200) {
+                setFormType("otp")
+            } else {
+                console.log(res)
+            }
+        }
+    }
+
+    const otpVerification = async () => {
+        try {
+            if (!otp) {
+                return
+            } else if (otp.length < 6) {
+                return
+            }
+
+            const res = await axios.post("api/auth/otp-verification", {
+                email,
+                otp
+            })
+
+            if (res.status === 200) {
+                try {
+                    await signIn(
+                        "credentials",
+                        {
+                            email,
+                            password,
+                            callbackUrl: "/",
+                            redirect: false
+                        }
+                    )
+                } catch (err) {
+                    console.log(err)
+                }
+            } else {
+                console.log(res.data)
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const resendOTP = async () => {
+        try {
+            const res = await axios.post("api/auth/resend-otp", { email })
+
+            if (res.status === 200) {
+                console.log("")
+            } else {
+                console.log(res)
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const handleSubmit = async () => {
+        if (formType === "otp") {
+            await otpVerification()
+        } else {
+            await loginRegisterHandler()
+        }
+    }
+
+    const handleClickableText = async () => {
+        if (formType === "otp") {
+            await resendOTP()
         }
         else toggleFormType()
     }
@@ -158,14 +251,14 @@ export const SignupModal = ({ isVisible, onClose }) => {
     }
     return <div className="fixed inset-0 bg-black bg-opacity-80 backdrop:blur-md text-slate-300 flex justify-center items-center">
         <div className="w-full sm:w-2/3 md:w-1/3 bg-slate-900 bg-opacity-60 rounded-2xl p-4 md:p-6">
-            <div className="relative flex flex-col justify-center gap-3 pb-4 mb-4" >
+            <form className="relative flex flex-col justify-center gap-3 pb-4 mb-4" >
                 <X onClick={onClose} className="absolute top-2 right-2 cursor-pointer" />
                 <div className="flex justify-center text-4xl font-extrabold text-violet-500">{FORM_TYPE[formType].formTitle}</div>
                 <div className="p-4 mb-4">Welcome to the Club mate!!</div>
 
                 <FormComponent formType={formType} username={username} setUsername={setUsername} email={email} setEmail={setEmail} password={password} setPassword={setPassword} otp={otp} setOtp={setOtp} />
 
-                <button class="w-full mt-4 rounded-md bg-gradient-to-tr from-slate-200 to-amber-700 py-2 border-2 border-slate-500 px-2  text-center text-sm text-white " type="button">
+                <button class="w-full mt-4 rounded-md bg-gradient-to-tr from-slate-200 to-amber-700 py-2 border-2 border-slate-500 px-2  text-center text-sm text-white " type="button" onSubmit={handleSubmit}>
                     {FORM_TYPE[formType].submitBtnTxt}   </button>
                 <label className="text-center text-sm">
                     {FORM_TYPE[formType].clickableTxtMsg}
@@ -173,7 +266,7 @@ export const SignupModal = ({ isVisible, onClose }) => {
                         {FORM_TYPE[formType].clickableTxt}
                     </span>
                 </label>
-            </div>
+            </form>
         </div>
     </div>
 }
