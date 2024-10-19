f76eadf279220f08ee805547262a2e2f70b32af8
10-abhi <abhishek81936@gmail.com>
2024-10-19 11:52:23 +0530

    changing txt

--- components/home.jsx
+++ components/home.jsx
@@ -17,7 +17,7 @@ export const HomePage = () => {
                 </div>
             </div>
             <div className="text-slate-300 text-center text-xl mt-4 pb-4">
-                this is small text idk added anything
+                Great Power comes with great responsibilties
             </div>
             <div>
                 <button type="button" onClick={()=>setisVisible(true)} className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-violet-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
